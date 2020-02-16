d8c7aee193aa271febcd8552c5b25e05eb32d847
Daniel Draskov <ddraskovv@gmail.com>
2020-02-16 22:49:40 +0200

    firebase posts

--- ng-blog/src/app/app.module.ts
+++ ng-blog/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFireModule } from 'angularfire2';
@@ -12,6 +12,12 @@ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { PostsModule } from './posts/posts.module';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/blog', pathMatch: 'full'},
+  {path: '',loadChildren: './posts/posts.module#PostsModule'}
+]
 
 @NgModule({
   declarations: [
@@ -25,7 +31,8 @@ import { SharedModule } from './shared/shared.module';
     AngularFireAuthModule,
     AngularFireStorageModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    PostsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
