e974ac9103c155b704eaa673511988ccb71e5519
Daniel Draskov <ddraskovv@gmail.com>
2020-02-17 12:59:44 +0200

    Create input form test

--- ng-blog/src/app/material.module.ts
+++ ng-blog/src/app/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule],
-  exports: [MatButtonModule, MatToolbarModule]
+  imports: [MatButtonModule, MatExpansionModule, MatToolbarModule, MatInputModule],
+  exports: [MatButtonModule, MatExpansionModule, MatToolbarModule, MatInputModule]
 })
 export class MaterialModule { }
