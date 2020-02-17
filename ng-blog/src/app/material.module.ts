fb4e18a8ee19746292b6c1f34f9eeb83d992b190
Daniel Draskov <ddraskovv@gmail.com>
2020-02-17 14:35:08 +0200

    Image upload

--- ng-blog/src/app/material.module.ts
+++ ng-blog/src/app/material.module.ts
@@ -3,9 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
-  imports: [MatButtonModule, MatExpansionModule, MatToolbarModule, MatInputModule],
-  exports: [MatButtonModule, MatExpansionModule, MatToolbarModule, MatInputModule]
+  imports: [MatButtonModule, MatDividerModule,MatProgressBarModule,MatListModule, MatExpansionModule, MatToolbarModule, MatInputModule],
+  exports: [MatButtonModule, MatDividerModule,MatProgressBarModule,MatListModule, MatExpansionModule, MatToolbarModule, MatInputModule]
 })
 export class MaterialModule { }
--- ng-blog/src/app/posts/post-dashboard/post-dashboard.component.ts
+++ ng-blog/src/app/posts/post-dashboard/post-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/auth.service';
 import { PostService } from '../post.service';
+import { Observable } from 'rxjs';
+import { AngularFireStorage } from 'angularfire2/storage';
 
 @Component({
   selector: 'app-post-dashboard',
@@ -9,11 +11,16 @@ import { PostService } from '../post.service';
 })
 export class PostDashboardComponent implements OnInit {
 
-  title: string
-  image: string = null
-  content: string
+  title: string;
+  image: string = null;
+  content: string;
 
-  constructor(private auth: AuthService, private postService: PostService) { }
+  buttonText: string = "Create Post";
+
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+
+  constructor(private auth: AuthService, private postService: PostService, private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
   }
@@ -28,6 +35,26 @@ export class PostDashboardComponent implements OnInit {
       title: this.title
     }
     this.postService.create(data);
+    this.title = '';
+    this.content = '';
+    this.buttonText = "Post Created!";
+    setTimeout(()=> this.buttonText = "Create Post" , 3000);
+  }
+
+  uploadImage(event){
+    const file = event.target.files[0];
+    const path = `posts/${file.name}`;
+
+    if(file.type.split('/')[0] !== 'image'){
+      return alert("only image files")
+    }
+    else
+    {
+      const task = this.storage.upload(path, file);
+      this.downloadURL = task.downloadURL();
+      this.uploadPercent = task.percentageChanges();
+      this.downloadURL.subscribe(url => this.image = url);
+    }
   }
 
 }
